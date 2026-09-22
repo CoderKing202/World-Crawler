942091a233de227170100549f2ab0ec6dabab050
Jatin <codeking202@gmail.com>
2026-09-22 11:45:50 +0530

    Top Navbar and manu inside it added for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <div>
+        <header>
+            <nav className="navbar">
+              <div id="logo">
+              <a href="/">
+                <img src="/logo.png" height={100} alt="World Crawler logo" />
+              </a>
+              </div>
+              <div id="menu">
+                <a href="/">Home</a>
+                <a href="/reels">Reels</a>
+                <a href="/chat">Chat</a>
+                <a href="/profile">Profile</a>
+              </div>
+              <div id="notification">
+                <img
+                  src="/notification_bell.png"
+                  alt="notification bell"
+                  height={100}
+                />
+              </div>
+            </nav>
+          
+        </header>
+        <main></main>
+        <footer></footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
